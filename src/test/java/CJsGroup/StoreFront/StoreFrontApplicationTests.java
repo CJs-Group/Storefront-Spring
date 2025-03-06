c2b1ae5a2d7cc531b6c84b07534d987ba507bceb
CJ <88186343+CJKorn@users.noreply.github.com>
2025-03-06 13:11:15 +1100

    Add Spring Template

---
+++ src/test/java/CJsGroup/StoreFront/StoreFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package CJsGroup.StoreFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
